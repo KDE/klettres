d376806716674fb8facc3c27bb2f1a3ca83d5eaf
Anne-Marie Mahfouf <annma@kde.org>
2005-08-27 02:04:35 +0000

    TRUE->true fix copyrights
    svn path=/branches/KDE/3.5/kdeedu/klettres/; revision=453766

--- klettres/klnewstuff.h
+++ klettres/klnewstuff.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2001-2004 Anne-Marie Mahfouf <annma@kde.org>
  *  Copyright (c) 2002 Cornelius Schumacher <schumacher@kde.org>
+ *  Copyright (C) 2004-2005 Anne-Marie Mahfouf <annma@kde.org>
  *
     This program is free software; you can redistribute it and/or
     modify it under the terms of version 2 of the GNU General Public
