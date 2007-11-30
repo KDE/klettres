94fc8c2c1d4c3a6b24e6020c34a134ed86887177
Tom Albers <toma@kde.org>
2007-11-30 20:00:41 +0000

    RELICENSE: all copyright holders/contributors agree to a relicense from GPL2 to GPL2+
    svn path=/trunk/KDE/kdeedu/klettres/; revision=743375

--- src/langutils.cpp
+++ src/langutils.cpp
@@ -1,9 +1,11 @@
 /*
-   Copyright (C) 2007      Pino Toscano <pino@kde.org>
-                                        Anne-Marie Mahfouf <annma@kde.org>
+    Copyright 2007 Pino Toscano <pino@kde.org>
+    Copyright 2007 Anne-Marie Mahfouf <annma@kde.org>
+
     This program is free software; you can redistribute it and/or
-    modify it under the terms of version 2 of the GNU General Public
-    License as published by the Free Software Foundation.
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
