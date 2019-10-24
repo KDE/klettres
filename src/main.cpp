c4db88a97eaa3d879363c2a2060bad87a09fc955
Laurent Montel <montel@kde.org>
2019-10-24 06:54:42 +0200

    GIT_SILENT: it compiles fine without deprecated method

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,7 @@ int main(int argc, char **argv)
 
     // see if we are starting with session management
     if (app.isSessionRestored()) {
-        RESTORE(KLettres)
+        kRestoreMainWindows<KLettres>();
     } else {
         // no session.. just start up normally
         if (parser.positionalArguments().isEmpty()) {
